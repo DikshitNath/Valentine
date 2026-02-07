fcce2fa2cac8c03f1a90e6c7a73bd699d74f3032
DikshitNath <dikshitnath36@gmail.com>
2026-02-07 23:15:57 +0530

    accept.html file created

--- script.js
+++ script.js
@@ -4,17 +4,7 @@ const container = document.querySelector(".container");
 
 // Romantic ending 💖
 yes.addEventListener("click", () => {
-  document.body.innerHTML = `
-    <div style="position:fixed;top:0;left:0;width:100%;height:100%;padding:20px;background-color:#ffe6e6;display:flex;flex-direction:column;align-items:center;justify-content:center;">
-      <h1 style="text-align:center;">
-        💖 I knew you’d say YES 💖
-      </h1>
-      <img src="./dudu-kiss-bubu-kisses.gif">
-      <h1 style="text-align:center; font-size:1.8rem; margin-top:20px;">
-        💖 LOVE YOU FOREVER 💖
-    </h1>
-    </div>
-  `;
+  document.location.href = "accept.html";
 });
 
 
